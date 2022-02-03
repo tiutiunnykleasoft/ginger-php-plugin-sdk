c031805acff4a2cab7afc18a141a80572ce6984f
Jorn Oomen <jorn@jornoomen.nl>
2022-02-03 09:01:09 +0100

    Use constructor injection and remove the obsolete setters

--- src/Entities/Customer.php
+++ src/Entities/Customer.php
@@ -16,25 +16,25 @@ class Customer implements MultiFieldsEntityInterface
     use FieldsValidatorTrait;
 
     private array $fields;
-    private Address $address;
-    private AbstractCollection|null $phone_numbers = null;
-    private BaseField|null $merchant_customer_id = null;
-    private BaseField|null $email_address = null;
-    private BaseField|null $first_name = null;
-    private BaseField|null $last_name = null;
-    private string|null $address_type = null;
-    private string|null $postal_code = null;
-    private string|null $housenumber = null;
-    private string|null $country = null;
-    private BaseField|null $locale = null;
-    private BaseField|null $ip_address = null;
-    private BaseField|null $gender = null;
-    private BaseField|null $birthdate = null;
-    private BaseField|null $address_line;
 
-    public function __construct()
-    {
-        $this->address = new Address();
+    public function __construct(
+        private ?Address $address = null,
+        private array $phone_numbers = [],
+        private ?string $merchant_customer_id = null,
+        private ?string $email_address = null,
+        private ?string $first_name = null,
+        private ?string $last_name = null,
+        private ?string $address_type = null,
+        private ?string $postal_code = null,
+        private ?string $housenumber = null,
+        private ?string $country = null,
+        private ?string $locale = null,
+        private ?string $ip_address = null,
+        private ?string $gender = null,
+        private ?string $birthdate = null,
+        private ?string $address_line = null,
+    ) {
+        $this->address = $this->address ?? new Address();
         $this->fields = [
             "merchant_customer_id" => false,
             "email_address" => false,
@@ -54,46 +54,12 @@ class Customer implements MultiFieldsEntityInterface
     }
 
     /** -------------------------------- Reworked ------------------------------- */
-
-    /**
-     * @param string $name
-     * @return $this
-     */
-    public function setFirstName(string $name): static
-    {
-        $this->first_name = new class extends BaseField {
-            #[Pure] public function __construct()
-            {
-                parent::__construct('first_name');
-            }
-        };
-        $this->first_name->set($name);
-
-        return $this;
-    }
-
     /**
      * @return string|null
      */
     #[Pure] public function getFirstName(): ?string
     {
-        return $this->first_name?->get();
-    }
-
-    /**
-     * @param string $name
-     * @return $this
-     */
-    public function setLastName(string $name): static
-    {
-        $this->last_name = new class extends BaseField {
-            #[Pure] public function __construct()
-            {
-                parent::__construct('last_name');
-            }
-        };
-        $this->last_name->set($name);
-        return $this;
+        return $this->first_name;
     }
 
     /**
@@ -101,23 +67,7 @@ class Customer implements MultiFieldsEntityInterface
      */
     #[Pure] public function getLastName(): ?string
     {
-        return $this->last_name?->get();
-    }
-
-    /**
-     * @param string $address
-     * @return $this
-     */
-    public function setEmailAddress(string $address): static
-    {
-        $this->email_address = new class extends BaseField {
-            #[Pure] public function __construct()
-            {
-                parent::__construct('email_address');
-            }
-        };
-        $this->email_address->set($address);
-        return $this;
+        return $this->last_name;
     }
 
     /**
@@ -125,18 +75,7 @@ class Customer implements MultiFieldsEntityInterface
      */
     #[Pure] public function getEmailAddress(): ?string
     {
-        return $this->email_address?->get();
-    }
-
-    /**
-     * @param string $country
-     * @return $this
-     */
-    public function setCountry(string $country): static
-    {
-        $this->address->setCountry($country);
-        $this->country = $this->address->getCountry();
-        return $this;
+        return $this->email_address;
     }
 
     /**
@@ -148,17 +87,6 @@ class Customer implements MultiFieldsEntityInterface
     }
 
     /**
-     * @param string $type
-     * @return $this
-     */
-    public function setAddressType(string $type): static
-    {
-        $this->address->setAddressType($type);
-        $this->address_type = $this->address->getAddressType();
-        return $this;
-    }
-
-    /**
      * @return string|null
      */
     public function getAddressType(): ?string
@@ -167,44 +95,11 @@ class Customer implements MultiFieldsEntityInterface
     }
 
     /**
-     * @param string $date
-     * @return $this
-     */
-    public function setBirthdate(string $date): static
-    {
-        $this->birthdate = new class extends BaseField implements ValidateFieldsInterface {
-
-            #[Pure] public function __construct()
-            {
-                parent::__construct('birthdate');
-            }
-
-            public function validate()
-            {
-                // TODO: Implement validate() method.
-            }
-        };
-        $this->birthdate->set($date);
-        return $this;
-    }
-
-    /**
      * @return string
      */
     #[Pure] public function getBirthdate(): string
     {
-        return $this->birthdate?->get();
-    }
-
-    /**
-     * @param string $number
-     * @return $this
-     */
-    public function setHousenumber(string $number): static
-    {
-        $this->address->setHousenumber($number);
-        $this->housenumber = $this->address->getHousenumber();
-        return $this;
+        return $this->birthdate;
     }
 
     /**
@@ -216,55 +111,11 @@ class Customer implements MultiFieldsEntityInterface
     }
 
     /**
-     * @param string $gender
-     * @return $this
-     */
-    public function setGender(string $gender): static
-    {
-        $this->gender = new class extends BaseField implements ValidateFieldsInterface {
-
-            #[Pure] public function __construct()
-            {
-                parent::__construct('gender');
-            }
-
-            public function validate()
-            {
-                // TODO: Implement validate() method.
-            }
-        };
-        $this->gender->set($gender);
-        return $this;
-    }
-
-    /**
      * @return string
      */
     #[Pure] public function getGender(): string
     {
-        return $this->gender?->get();
-    }
-
-    /**
-     * @param string $locale
-     * @return $this
-     */
-    public function setLocale(string $locale): static
-    {
-        $this->locale = new class extends BaseField implements ValidateFieldsInterface {
-
-            #[Pure] public function __construct()
-            {
-                parent::__construct('locale');
-            }
-
-            public function validate()
-            {
-                // TODO: Implement validate() method.
-            }
-        };
-        $this->locale->set($locale);
-        return $this;
+        return $this->gender;
     }
 
     /**
@@ -272,23 +123,7 @@ class Customer implements MultiFieldsEntityInterface
      */
     #[Pure] public function getLocale(): ?string
     {
-        return $this->locale?->get();
-    }
-
-    /**
-     * @param string $address
-     * @return $this
-     */
-    public function setIpAddress(string $address): static
-    {
-        $this->ip_address = new class extends BaseField {
-            #[Pure] public function __construct()
-            {
-                parent::__construct('ip_address');
-            }
-        };
-        $this->ip_address->set($address);
-        return $this;
+        return $this->locale;
     }
 
     /**
@@ -296,23 +131,7 @@ class Customer implements MultiFieldsEntityInterface
      */
     #[Pure] public function getIpAddress(): string
     {
-        return $this->ip_address->get();
-    }
-
-    /**
-     * @param string $id
-     * @return $this
-     */
-    public function setMerchantCustomerId(string $id): static
-    {
-        $this->merchant_customer_id = new class extends BaseField {
-            #[Pure] public function __construct()
-            {
-                parent::__construct('merchant_customer_id');
-            }
-        };
-        $this->merchant_customer_id->set($id);
-        return $this;
+        return $this->ip_address;
     }
 
     /**
@@ -320,18 +139,7 @@ class Customer implements MultiFieldsEntityInterface
      */
     #[Pure] public function getMerchantCustomerId(): ?string
     {
-        return $this->merchant_customer_id->get();
-    }
-
-    /**
-     * @param string $code
-     * @return $this
-     */
-    public function setPostalCode(string $code): static
-    {
-        $this->address->setPostalCode($code);
-        $this->postal_code = $this->address->getPostalCode();
-        return $this;
+        return $this->merchant_customer_id;
     }
 
     /**
@@ -343,20 +151,9 @@ class Customer implements MultiFieldsEntityInterface
     }
 
     /**
-     * @param \GingerPluginSdk\Collections\AbstractCollection $phone_numbers
-     * @return \GingerPluginSdk\Entities\Customer
+     * @return string[]
      */
-    public function setPhoneNumbers(AbstractCollection $phone_numbers): static
-    {
-        if ($phone_numbers->count() < 1) $phone_numbers = null;
-        $this->phone_numbers = $phone_numbers;
-        return $this;
-    }
-
-    /**
-     * @return \GingerPluginSdk\Collections\AbstractCollection|null
-     */
-    public function getPhoneNumbers(): ?AbstractCollection
+    public function getPhoneNumbers(): array
     {
         return $this->phone_numbers;
     }
@@ -365,27 +162,9 @@ class Customer implements MultiFieldsEntityInterface
      * @TODO: The last 4 methods is not proofed, just a mock. Take a look!
      * @param \GingerPluginSdk\Entities\Address $address
      */
-    public function setAddress(Address $address): static
-    {
-        $this->address = $address;
-        return $this;
-    }
-
-    public function setAddressLine(): static
-    {
-        $this->address_line = new class extends BaseField {
-            public function __construct()
-            {
-                parent::__construct('address_line');
-            }
-        };
-        $this->address_line->set($this->address->generateAddress());
-        return $this;
-    }
-
     #[Pure] public function getAddressLine()
     {
-        return $this->address_line->get();
+        return $this->address_line;
     }
 
     #[Pure] public function getAddress(): Address
