f760cd295d9231fbe11eb35c09375cc6cebf046a
sorshireality <tutunikssa@gmail.com>
2022-02-10 15:22:26 +0200

    - Added Currency.

---
+++ src/Entities/Currency.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace GingerPluginSdk\Entities;
+
+use GingerPluginSdk\Bases\BaseField;
+use GingerPluginSdk\Exceptions\OutOfEnumException;
+use GingerPluginSdk\Interfaces\ValidateFieldsInterface;
+
+final class Currency extends BaseField implements ValidateFieldsInterface
+{
+    /**
+     * @throws \GingerPluginSdk\Exceptions\OutOfEnumException
+     */
+    public function validate($value)
+    {
+        //@TODO: IMPLEMENT CACHING CURRENCY LIST AND USE THERE.
+        $available_list = [];
+        if (in_array($value, $available_list)) {
+            throw new OutOfEnumException(key: $this->getPropertyName(), real_value: $value, expected_value: json_encode($available_list));
+        }
+    }
+
+    public function __construct($value)
+    {
+        parent::__construct('currency');
+        $this->set($value);
+    }
+}
